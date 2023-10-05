2c4ac135060784b23c0a049cfc9fd618553dba8e
isaacmain254 <isaacmain72@gmail.com>
2023-10-05 22:04:41 +0300

    integrated stripe API

--- myshop/settings.py
+++ myshop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
+    'payment.apps.PaymentConfig',
 ]
 
 MIDDLEWARE = [
@@ -137,3 +138,9 @@ NEDIA_ROOT = BASE_DIR / 'media'
 CART_SESSION_ID = 'cart'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# STRIPE CONFIGURATIONS
+STRIPE_PUBLISHABLE_KEY = 'pk_test_51NxZVrLeOSiFv2tSB6y6CWZfyjMGq14NLicGDQWoJNtdiWE9tCdxogb5Dd35L3ZyZGHNSThjH5kNtV2po0mVQ0cd00yGK8eR7n'
+STRIPE_SECRET_KEY = 'sk_test_51NxZVrLeOSiFv2tS836x4B4TdKxd6decXlZLLQ4iOT3fa4szywswhSNy1dvQJCtDqE3b6VyiQhOTNGcLawxIOzvn00IN16rRk2'
+STRIPE_API_VERSION = '2022-08-01'
+STRIPE_WEBHOOK_SECRET = 'whsec_eb3b623c4a74014c4e4d0eb5be96c21776ab8d936bc8ccac2aec701796cdff3d'
